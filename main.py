ca17e499b0af6829f2e1fa35cfb0b412fd59cd33
Chi Akasiem <Lizchiomak@gmail.com>
2024-05-03 12:44:18 -0400

    Data wrangling - input missing values & change date format & round numeric values to 2 decimal places

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ for col in numeric_cols:
     # Check data types again after conversion
 print("\nData types after conversion:")
 print(df.info())
+
+# Handle missing values
+print("\nMissing values before handling:")
+print(df.isnull().sum())
+
+# Data Wrangling
+# Impute missing values with the mean of each column
+numeric_cols = df.select_dtypes(include=['number']).columns
+df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+df = df.round(2)
+
+# Recheck for missing values after imputation
+print("\nMissing values after imputation:")
+print(df.isnull().sum())
+
+# Convert 'Date' column to datetime format
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Save as CSV
+df.to_csv('data/processed_data/clean_data.csv', index=False)
